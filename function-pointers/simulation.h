75100456de54d12be502f0de5695507f1423ac93
owenjohnsons <owen.johnson@ucdconnect.ie>
2022-12-29 12:17:12 +0000

    Intial attempt.

--- function-pointers/simulation.h
+++ function-pointers/simulation.h
@@ -23,4 +23,57 @@ void build_corr_mat(double *const mat, const double rho, const int dim);
 void transform_to_correlated(double const *const C, double *const correlated, const int dim);
 void cholesky_decompose(double *const matrix_data, const int dim);
 
+/* Function Definitions */
+
+get_seed_from_random_device()
+{
+    FILE *fp;
+    unsigned long seed;
+    fp = fopen("/dev/urandom", "r");
+    fread(&seed, sizeof(seed), 1, fp);
+    fclose(fp);
+    return seed;
+}
+
+void populate_std_gaussian_rvs(double *const rngs, const long long int n)
+{
+    gsl_rng *r = gsl_rng_alloc(gsl_rng_mt19937);
+    gsl_rng_set(r, get_seed_from_random_device());
+    for (int i = 0; i < n; ++i) {
+        rngs[i] = gsl_ran_ugaussian(r);
+    }
+    gsl_rng_free(r);
+}
+
+build_corr_mat(double *const mat, const double rho, const int dim)
+{
+    for (int i = 0; i < dim; ++i) {
+        for (int j = 0; j < dim; ++j) {
+            if (i == j) {
+                mat[i * dim + j] = 1.0;
+            } else {
+                mat[i * dim + j] = rho;
+            }
+        }
+    }
+}
+
+transform_to_correlated(double const *const C, double *const correlated, const int dim)
+{
+    double *L = (double *)malloc(dim * dim * sizeof(double));
+    memcpy(L, C, dim * dim * sizeof(double));
+    cholesky_decompose(L, dim);
+    double *rngs = (double *)malloc(dim * sizeof(double));
+    populate_std_gaussian_rvs(rngs, dim);
+    cblas_dgemv(CblasRowMajor, CblasNoTrans, dim, dim, 1.0, L, dim, rngs, 1, 0.0, correlated, 1);
+    free(L);
+    free(rngs);
+}
+
+cholesky_decompose(double *const matrix_data, const int dim)
+{
+    int info;
+    LAPACKE_dpotrf(LAPACK_ROW_MAJOR, 'U', dim, matrix_data, dim);
+}
+
 #endif /* end of include guard: SIMULATION_H_WKIRHZEZ */
--- function-pointers/stats.h
+++ function-pointers/stats.h
@@ -8,10 +8,10 @@ double calculate_variance(double const *const val, const int n);
 /* Function Definitions */
 void portfolio_sums(double const * vals, double *const port_vals, const int dim, const int num_sims)
 {
-    for (int i = 0; i < num_sims; ++i) {
-        double sum = 0.0;
-        for (int j = 0; j < dim; ++j) {
-            sum += vals[i * dim + j];
+    for (int i = 0; i < num_sims; ++i) { // Loop over simulations
+        double sum = 0.0;                  
+        for (int j = 0; j < dim; ++j) {  // Sum of portfolio values
+            sum += vals[i * dim + j]; 
         }
         port_vals[i] = sum;
     }
@@ -28,10 +28,10 @@ double calculate_mean(double const *const val, const int n)
 
 double calculate_variance(double const *const val, const int n)
 {
-    double mean = calculate_mean(val, n);
-    double sum = 0.0;
+    double mean = calculate_mean(val, n); // Calling back mean function
+    double sum = 0.0; // Sum of squares
     for (int i = 0; i < n; ++i) {
-        sum += (val[i] - mean) * (val[i] - mean);
+        sum += (val[i] - mean) * (val[i] - mean); // (x - mean)^2
     }
     return sum / n;
 }
