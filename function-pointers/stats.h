b2a23be0c64bbb7ef2b0fed1f38622bfe543ddf2
owenjohnsons <owen.johnson@ucdconnect.ie>
2022-12-29 11:59:21 +0000

    Initial Attempt

--- function-pointers/stats.h
+++ function-pointers/stats.h
@@ -5,4 +5,35 @@ void portfolio_sums(double const * vals, double *const port_vals, const int dim,
 double calculate_mean(double const *const val, const int n);
 double calculate_variance(double const *const val, const int n);
 
+/* Function Definitions */
+void portfolio_sums(double const * vals, double *const port_vals, const int dim, const int num_sims)
+{
+    for (int i = 0; i < num_sims; ++i) {
+        double sum = 0.0;
+        for (int j = 0; j < dim; ++j) {
+            sum += vals[i * dim + j];
+        }
+        port_vals[i] = sum;
+    }
+}
+
+double calculate_mean(double const *const val, const int n)
+{
+    double sum = 0.0;
+    for (int i = 0; i < n; ++i) {
+        sum += val[i];
+    }
+    return sum / n;
+}
+
+double calculate_variance(double const *const val, const int n)
+{
+    double mean = calculate_mean(val, n);
+    double sum = 0.0;
+    for (int i = 0; i < n; ++i) {
+        sum += (val[i] - mean) * (val[i] - mean);
+    }
+    return sum / n;
+}
+
 #endif /* end of include guard: STATS_H_5BZQYSIS */
